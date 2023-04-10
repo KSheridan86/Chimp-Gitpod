669b5ad86ea01603c02d26cc50afc8be500af250
Ken Sheridan <kensheridan86@gmail.com>
2023-04-10 13:52:11 +0000

    base template added

--- HOME/urls.py
+++ HOME/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('projects/', views.projects, name="projects"),
     path('', views.Home_Page, name="home-page"),
 ]
--- HOME/views.py
+++ HOME/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
 
-# def projects(request):
-#     return render(request, 'base.html')
-
-
 def Home_Page(request):
     pk = 'Hello'
     context = {
